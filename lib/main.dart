a81ec6d21400c21572405062971b96a82d66d81d
Zvikasan <zvikasan_theonly_one@yahoo.com>
2021-03-15 11:42:25 +1100

    added controls

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pedometer/pedometer.dart';
 import 'dart:async';
 import 'package:assets_audio_player/assets_audio_player.dart';
@@ -49,6 +50,9 @@ class _MyHomePageState extends State<MyHomePage> {
   double playbackRate = 1.35;
   double testVar = 0.3;
   ValueListenable<double> playbackValue;
+  int lengthOfMeasurement = 20;
+  int periodOfMeasurement = 5;
+  int songBPM = 129;
 
   @override
   void initState() {
@@ -101,11 +105,11 @@ class _MyHomePageState extends State<MyHomePage> {
   calculateStepsPerMinute() async {
     int startSteps = _steps;
     int endSteps = 0;
-    await Future.delayed(const Duration(seconds: 20), () {});
+    await Future.delayed(Duration(seconds: lengthOfMeasurement), () {});
     endSteps = _steps - startSteps;
     setState(() {
       stepsPerMinute = endSteps * 6;
-      playbackRate = stepsPerMinute / 129; //129 is my sample song BPM
+      playbackRate = stepsPerMinute / songBPM; //129 is my sample song BPM
       assetsAudioPlayer.setPlaySpeed(playbackRate);
     });
     // advancedPlayer.setPlaybackRate(playbackRate: _playbackRate);
@@ -113,8 +117,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void startMeasuring() {
     // Timer timer;
-    Timer.periodic(
-        Duration(seconds: 5), (Timer t) => calculateStepsPerMinute());
+    Timer.periodic(Duration(seconds: periodOfMeasurement),
+        (Timer t) => calculateStepsPerMinute());
   }
 
   // ---------------- Music-Related-Code ------------------
@@ -140,146 +144,211 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text('Pedometer example app'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 20),
-              Text(
-                'Steps taken:',
-                style: TextStyle(fontSize: 10),
-              ),
-              Text(
-                _stepsText,
-                style: TextStyle(fontSize: 10),
-              ),
-              Divider(
-                //height: 100,
-                thickness: 2,
-                color: Colors.blueAccent,
-              ),
-              Text(
-                'SPM: ${stepsPerMinute.toString()}',
-                style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.blueAccent,
-                    fontWeight: FontWeight.bold),
-              ),
-              Divider(
-                // height: 100,
-                thickness: 2,
-                color: Colors.blueAccent,
-              ),
-              Text(
-                'Pedestrian status:',
-                style: TextStyle(fontSize: 10),
-              ),
-              Icon(
-                _status == 'walking'
-                    ? Icons.directions_walk
-                    : _status == 'stopped'
-                        ? Icons.accessibility_new
-                        : Icons.error,
-                size: 10,
-              ),
-              Center(
-                child: Text(
-                  _status,
-                  style: _status == 'walking' || _status == 'stopped'
-                      ? TextStyle(fontSize: 30)
-                      : TextStyle(fontSize: 20, color: Colors.red),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Text(
+                  'Steps taken:',
+                  style: TextStyle(fontSize: 10),
                 ),
-              ),
-              Divider(
-                //height: 100,
-                thickness: 2,
-                color: Colors.blueAccent,
-              ),
-              Text(
-                'Playback Rate $playbackRate',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              Divider(
-                //height: 100,
-                thickness: 2,
-                color: Colors.blueAccent,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer
-                            .open(Audio('assets/music/i_like_it.mp3'));
-                      },
-                      child: Text('OpenSong')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.play();
-                      },
-                      child: Text('Play')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.stop();
-                      },
-                      child: Text('NewPlayerStop')),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(0.5);
-                      },
-                      child: Text('x0.5')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(1.0);
-                      },
-                      child: Text('x1.0')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(1.5);
-                      },
-                      child: Text('x1.5')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(2.0);
-                      },
-                      child: Text('x2.0')),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(0.23);
-                      },
-                      child: Text('x0.23')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(1.67);
-                      },
-                      child: Text('x1.67')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(1.23);
-                      },
-                      child: Text('x1.23')),
-                  ElevatedButton(
-                      onPressed: () {
-                        assetsAudioPlayer.setPlaySpeed(2.34);
-                      },
-                      child: Text('x2.34')),
-                ],
-              ),
-              Divider(
-                //height: 100,
-                thickness: 2,
-                color: Colors.blueAccent,
-              ),
-            ],
+                Text(
+                  _stepsText,
+                  style: TextStyle(fontSize: 10),
+                ),
+                Divider(
+                  //height: 100,
+                  thickness: 2,
+                  color: Colors.blueAccent,
+                ),
+                Text(
+                  'SPM: ${stepsPerMinute.toString()}',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold),
+                ),
+                Divider(
+                  // height: 100,
+                  thickness: 2,
+                  color: Colors.blueAccent,
+                ),
+                Text(
+                  'Pedestrian status:',
+                  style: TextStyle(fontSize: 10),
+                ),
+                Icon(
+                  _status == 'walking'
+                      ? Icons.directions_walk
+                      : _status == 'stopped'
+                          ? Icons.accessibility_new
+                          : Icons.error,
+                  size: 10,
+                ),
+                Center(
+                  child: Text(
+                    _status,
+                    style: _status == 'walking' || _status == 'stopped'
+                        ? TextStyle(fontSize: 30)
+                        : TextStyle(fontSize: 20, color: Colors.red),
+                  ),
+                ),
+                Divider(
+                  //height: 100,
+                  thickness: 2,
+                  color: Colors.blueAccent,
+                ),
+                Text(
+                  'Playback Rate $playbackRate',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                Divider(
+                  //height: 100,
+                  thickness: 2,
+                  color: Colors.blueAccent,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer
+                              .open(Audio('assets/music/i_like_it.mp3'));
+                        },
+                        child: Text('OpenSong')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.play();
+                        },
+                        child: Text('Play')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.stop();
+                        },
+                        child: Text('NewPlayerStop')),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(0.5);
+                        },
+                        child: Text('x0.5')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(1.0);
+                        },
+                        child: Text('x1.0')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(1.5);
+                        },
+                        child: Text('x1.5')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(2.0);
+                        },
+                        child: Text('x2.0')),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(0.23);
+                        },
+                        child: Text('x0.23')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(1.67);
+                        },
+                        child: Text('x1.67')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(1.23);
+                        },
+                        child: Text('x1.23')),
+                    ElevatedButton(
+                        onPressed: () {
+                          assetsAudioPlayer.setPlaySpeed(2.34);
+                        },
+                        child: Text('x2.34')),
+                  ],
+                ),
+                Divider(
+                  //height: 100,
+                  thickness: 2,
+                  color: Colors.blueAccent,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 50,
+                      child: TextField(
+                        onSubmitted: (nums) {
+                          setState(() {
+                            lengthOfMeasurement = int.parse(nums);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(labelText: 'L of M'),
+                      ),
+                    ),
+                    Container(
+                      width: 50,
+                      child: TextField(
+                        onSubmitted: (nums) {
+                          setState(() {
+                            periodOfMeasurement = int.parse(nums);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(labelText: 'P of M'),
+                      ),
+                    ),
+                    Container(
+                      width: 90,
+                      child: TextField(
+                        onSubmitted: (nums) {
+                          setState(() {
+                            songBPM = int.parse(nums);
+                          });
+                        },
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(labelText: 'Song BPM'),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  //height: 100,
+                  thickness: 2,
+                  color: Colors.blueAccent,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Length: $lengthOfMeasurement'),
+                    Text('Period: $periodOfMeasurement'),
+                    Text('SongBPM: $songBPM')
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
